54f7c1bae4ded0c3be4eb79089f00093c180a95a
Nathan Pankowsky <nathan.pankowsky@gmail.com>
2017-09-19 21:46:31 +0300

    Added hpp file for formation and stub for formation test

---
+++ formation.hpp
@@ -0,0 +1,23 @@
+/*
+ * formation.hpp
+ *
+ *  Created on: Sep 19, 2017
+ *      Author: Nathan Pankowsky
+ */
+
+#ifndef FORMATION_HPP_
+#define FORMATION_HPP_
+
+#include "ship.hpp"
+
+namespace objects {
+
+class Formation: public Ship {
+public:
+	Formation();
+	virtual ~Formation();
+};
+
+} /* namespace objects */
+
+#endif /* FORMATION_HPP_ */
